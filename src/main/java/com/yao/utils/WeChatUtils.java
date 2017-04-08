d4021bd48095699d8638b6e2823cd2dfc98567fc
531416635 <531416635@qq.com>
2017-04-08 18:44:04 +0800

    添加一些微信参数

---
+++ src/main/java/com/yao/utils/WeChatUtils.java
@@ -0,0 +1,10 @@
+package com.yao.utils;
+
+public interface WeChatUtils {
+
+	public static final String Token ="xianglaiju";
+	public static final String appID ="wxec4ae298ee5742d7";
+	public static final String appsecret ="d4624c36b6795d1d99dcf0547af5443d";
+	
+	
+}
